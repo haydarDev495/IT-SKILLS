bdaec0923bc83377c40a431e378bede845200124
Haydar Bekmuradov <haydarbekmuradov50@gmail.com>
2022-09-19 16:47:32 +0300

    New Project Created

---
+++ IT-SKILLS/IT-SKILLS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IT-SKILLS
+//
+//  Created by Haydar Bekmuradov on 19.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
